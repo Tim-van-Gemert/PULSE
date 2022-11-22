7f969ef8e67ed388f99c0f416f0f4617e0720852
Y-Shinden King <yshinden.king@student.rocva.nl>
2022-11-22 11:17:02 +0100

    date time dark

--- Pages/index.php
+++ Pages/index.php
@@ -18,13 +18,13 @@
   
 </body>
 <script>
-    const date = new Date();
-let day = date.getDate();
-let month = date.getMonth() + 1;
-let year = date.getFullYear();
 
 // This arrangement can be altered based on how we want the date's format to appear.
-let currentDate = `${day}-${month}-${year}`;
-	document.getElementById("date").innerHTML = currentDate;
+let date = new Date();
+
+let day =  date.toLocaleDateString('en-US', {
+    weekday: 'long'
+  })
+	document.getElementById("date").innerHTML =  day;
 </script>
 </html>
