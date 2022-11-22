8b25eb71db227eb48f40a6aed705cf9d44a644f8
Y-Shinden King <yshinden.king@student.rocva.nl>
2022-11-22 11:55:42 +0100

    datetime

--- Pages/index.php
+++ Pages/index.php
@@ -7,23 +7,23 @@
 </head>
 
 <body class="dark:bg-black bg-white w-screen justify-around flex flex-col ">
+
     <nav class="flex flex-col  mt-14  ">
         <div class="justify-around w-screen flex" >
             <img class="dark:invert h-[50%] w-[50%] " src="../Media/PULSE__logo__1.png">
         </div>
-        <div class="justify-around w-screen mt-4  flex" >
+        <div class="justify-around w-screen mt-4 divide-y dark:divide-white  flex" >
         <h2 id="date" class="dark:text-white text-black  text-lg font-thin   " ></h2>
         </div>
     </nav>
-  
 </body>
 <script>
 
 // This arrangement can be altered based on how we want the date's format to appear.
 let date = new Date();
-
-let day =  date.toLocaleDateString('en-US', {
-    weekday: 'long'
+let day =  date.toDateString('en-US', {
+    weekday: 'long',
+    month: 'long'
   })
 	document.getElementById("date").innerHTML =  day;
 </script>
