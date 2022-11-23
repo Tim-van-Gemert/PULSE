c7ea6c5584314101aa5529da29a0640334e7b5de
Omer Ulan Uluu <omer.ulanuluu@student.rocva.nl>
2022-11-24 00:22:33 +0100

    starting to build survey pages

---
+++ Pages/mental-health-survey.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mental Health Survey</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="date.js" defer></script>
+</head>
+
+<body class="overflow-auto dark:bg-black bg-white h-[100vh]">
+    <img src="../Media/iPhone 13.png" alt="Statusbar">
+    <div class="flex m-6 justify-center flex-col">
+        <p id="date" class="dark:text-white text-black text-lg font-thin"></p>
+        <h1 class="text-4xl font-bold pb-2 mt-1 tracking-wider">Mental Health Survey</h1>
+        <div class="drop-shadow-lg rounded-lg w-55 h-48 bg-slate-100 rounded-2xl">
+
+        </div>
+        <div class="drop-shadow-lg rounded-lg w-55 h-24 bg-slate-100 rounded-2xl mt-6">
+
+        </div>
+        <div class="drop-shadow-lg rounded-lg w-55 h-32 bg-slate-100 rounded-2xl mt-6">
+
+        </div>
+
+    </div>
+
+    <?php
+    include('footer.php')
+    ?>
+    </footer>
+</body>
+
+</html>
--- Pages/surveys.php
+++ Pages/surveys.php
@@ -5,19 +5,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Survey</title>
+    <title>Surveys</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="date.js" defer></script>
 </head>
 
-<body class="dark:bg-black bg-white h-[100vh]">
+<body class="overflow-auto dark:bg-black bg-white h-[100vh]">
     <img src="../Media/iPhone 13.png" alt="Statusbar">
     <div class="flex m-6 justify-center flex-col">
         <p id="date" class="dark:text-white text-black text-lg font-thin"></p>
         <h1 class="text-5xl font-bold pb-7 mt-1 tracking-wider">Surveys</h1>
-        <div class="rounded-lg w-55 h-64 bg-slate-100 rounded-3xl">
+        <div class="drop-shadow-lg rounded-lg w-55 h-64 bg-slate-100 rounded-2xl">
             <h5 class="text-2xl font-bold font-mono pt-3 pl-3">RPE Survey</h5>
-            <p class="pt-4 pl-3">Let us know how your workout went.</p>
+            <p class="pt-3 pl-3">Let us know how your workout went.</p>
 
             <a href="#" class="w-32 h-14">
                 <div class="flex justify-center items-center ml-24 mt-5 w-36 h-14 bg-black rounded-xl">
@@ -30,9 +30,9 @@
                 <p class="font-bold text-slate-700">Survey reminder notification: <span class="font-normal">ON</span></p>
             </div>
         </div>
-        <div class="rounded-lg w-55 h-64 bg-slate-100 rounded-3xl mt-6">
+        <div class="drop-shadow-lg rounded-lg w-55 h-64 bg-slate-100 rounded-2xl mt-6">
             <h5 class="text-2xl font-bold font-mono pt-3 pl-3">Mental Health Survey</h5>
-            <p class="pt-4 pl-3">Let us know how you have been feeling <br> lately.</p>
+            <p class="pt-3 pl-3">Let us know how you have been feeling <br> lately.</p>
             <a href="#" class="w-32 h-14">
                 <div class="flex justify-center items-center ml-24 mt-0 w-36 h-14 bg-black rounded-xl">
                     <input type="button" value="Get started" class="text-white font-serif text-xl font-bold font-mono">
@@ -43,7 +43,6 @@
                 <p class="font-bold text-slate-700">Regularity: <span class="font-normal">after each training session</span></p>
                 <p class="font-bold text-slate-700">Survey reminder notification: <span class="font-normal">ON</span></p>
             </div>
-
         </div>
     </div>
 
