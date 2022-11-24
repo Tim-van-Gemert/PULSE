7ec31b1ce1a52b24361a2d7a800ffbea94cbb1b4
Tim van Gemert <Timvangemert@outlook.com>
2022-11-24 13:16:39 +0100

    rerouted missing chart image

--- Pages/report.php
+++ Pages/report.php
@@ -3,14 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="manifest" href="site.webmanifest">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="dark:bg-black mt-2  ">
-<?php include('header.php') ?> 
-    <span class="flex px-10 pt-0 pb-2 -mt-4 items-center justify-between ">
+<body class="dark:bg-black">
+    <span class="sticky top-0 z-50 bg-white" >
+        <img src="../Media/iPhone 13.png" class="w12 bg-white dark:bg-black">
+    </span>
+    <span class="flex px-10 pt-8 pb-2 justify-between ">
         <div>
             <h1 class="text-4xl font-extrabold leading-8 dark:text-white">Dave<br>Mason</h1>
         </div>
@@ -26,18 +27,18 @@
     <span class="flex px-6 py-3 items-center mx-5 justify-between rounded-lg ">
         <div>
             <h4 class=" font-bold text-center leading-4 dark:text-white">TRACK</h4>
-            <h5 class=" font-bold text-center text-sm text-slate-500 leading-3 ">Muscles/Nerves activity<h5>
+            <h5 class=" font-bold text-center text-sm text-[#848484] leading-3 ">Muscles/Nerves activity<h5>
         </div>
         <div>
             <h4 class="font-bold text-center leading-4 dark:text-white">WORK CAPACITY</h4>
-            <h5 class="font-bold text-center text-sm text-slate-500 leading-3 "> Acceptable: 92%</h5>
+            <h5 class="font-bold text-center text-sm text-[#848484] leading-3 "> Acceptable: 92%</h5>
         </div>
     </span>
     
     <span class="flex pl-10 pr-5 my-10 mx-5  items-center justify-between rounded-lg ">
         <div>
             <h4 class="font-bold leading-5 text-xl mb-1 dark:text-white" >Work <br> Capacity</h4>
-            <h5 class=" w-28 font-bold  text-sm text-slate-500 leading-3">This is a fake text with fake data and stuff</h5>
+            <h5 class=" w-28 font-bold  text-sm text-[#848484] leading-3">This is a fake text with fake data and stuff</h5>
         </div>
 
         <div>
@@ -56,7 +57,7 @@
     <span class="flex pl-10 pr-5 my-10 mx-5 py-6  items-center justify-between rounded-lg">
         <div class="">
             <h4 class=" font-bold leading-5 text-xl mb-1 dark:text-white" >PULSE <br> Index</h4>
-            <h5 class=" w-28 font-bold  text-sm text-slate-500 leading-3 ">Muscles/Nerves <br> activity and fake <br> data and stuff</h5>
+            <h5 class=" w-28 font-bold  text-sm text-[#848484] leading-3 ">Muscles/Nerves <br> activity and fake <br> data and stuff</h5>
         </div>
         <div>
             <img src="../Media/Charts/column_chart.png" class=" myColumns w-36 h-36">
