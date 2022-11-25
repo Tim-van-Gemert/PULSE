5e051e9c9bd30fbbf3bef6b823b6f3bd19ac6bb2
Y-Shinden King <yshinden.king@student.rocva.nl>
2022-11-25 10:05:38 +0100

    Moved date

--- Pages/header.php
+++ Pages/header.php
@@ -2,7 +2,7 @@
         <div class="ml-5 w-screen flex flex-row">
             <img href="index.php"  class="dark:invert h-5 w-auto " src="../Media/PULSE__logo__1.png">
         </div>
-        <div class=" w-screen  mr-5  mb-2 flex">
+        <div class=" w-screen ml-12    mb-2 flex">
             <h2 id="date" class="dark:text-white text-black  ml-6 text-lg font-thin   "></h2>
         </div>
     </nav>
